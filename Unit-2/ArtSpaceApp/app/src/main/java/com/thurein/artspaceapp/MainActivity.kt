717e4b39005f7501a8f71b8e5878fdbb46cd97dd
thurein <thureintun@acedatasystems.com>
2024-04-28 23:10:23 +0630

    scroallble list

--- Unit-2/ArtSpaceApp/app/src/main/java/com/thurein/artspaceapp/MainActivity.kt
+++ Unit-2/ArtSpaceApp/app/src/main/java/com/thurein/artspaceapp/MainActivity.kt
@@ -3,13 +3,27 @@ package com.thurein.artspaceapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.thurein.artspaceapp.ui.theme.ArtSpaceAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,6 +44,52 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun ArtSpace(modifier: Modifier=Modifier){
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentWidth(),
+        verticalArrangement = Arrangement.Center,
+       horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ArtworkWall()
+        ArtworkDescriptor()
+        DisplayController()
+    }
+}
+@Composable
+fun ArtworkWall(modifier: Modifier=Modifier){
+    Column(modifier = modifier
+        .fillMaxWidth()
+        .padding(100.dp)
+        .background(Color.Blue)) {
+        Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = null)
+    }
+}
+
+@Composable
+fun ArtworkDescriptor(modifier: Modifier=Modifier){
+    Column(modifier = modifier){
+        Text(text = "Artwork Title")
+        Text(text = "Artwork Artist(Year)")
+    }
+}
+
+@Composable
+fun DisplayController(modifier: Modifier=Modifier){
+    Row(modifier=Modifier,Arrangement.SpaceBetween) {
+        Button(onClick = { /*TODO*/ }) {
+                
+            Text(text = "Prvious")
+        }
+        Button(onClick = { /*TODO*/ }) {
+
+            Text(text = "Next")
+        }
+    }
+}
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
@@ -37,10 +97,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ArtSpaceAppTheme {
-        Greeting("Android")
+        ArtworkWall()
     }
 }
